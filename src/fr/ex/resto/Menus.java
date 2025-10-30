cd0c9221acd16b50b73e925b60a439ecc31b7d99
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-30 15:15:17 +0100

    ajout et améliorations des commentaires + docstring

--- src/fr/ex/resto/Menus.java
+++ src/fr/ex/resto/Menus.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 
 public class Menus {
 	
+	// We declare a private static Scanner to be shared across all methods in the Menus class
 	private static final Scanner scan = new Scanner(System.in);
+
+	
+	/**
+     * Displays a question with a list of options and returns the user's selected choice.
+     * If the input is invalid, a default value "Aucun(e)" is returned.
+     *
+     * @param question The question to display to the user
+     * @param options The list of available choices
+     * @return The selected option or "Aucun(e)" if the input is invalid
+     */
 	
 	public static String getChoice(String question, String[] options) {
 		System.out.println(question);
@@ -24,33 +35,36 @@ public class Menus {
 	}
 	
 	
-	public static String entryChoice() {
+	// This method prompts the user to choose an appetizer
+	public static String appetizerChoice() {
 		
 	    return getChoice("Que souhaitez-vous comme entrée :", new String[] {
 	        "Salade", "Soupe", "Quiche", "Aucune"
 	    });
 	}
 	
-	
+	// This method prompts the user to choose a main course
 	public static String mainCourseChoice() {
 	    return getChoice("Que souhaitez-vous comme plat :", new String[] {
 	        "Poulet", "Boeuf", "Poisson", "Végétarien", "Végan", "Aucun"
 	    });
 	}
 	
-	
+	// // This method prompts the user to choose an side dish
 	public static String sideChoice() {
 	    return getChoice("Que souhaitez-vous comme accompagnements :", new String[] {
 	        "Riz", "Pâtes", "Frites", "Légumes", "Aucun"
 	    });
 	}
 	
+	// // This method prompts the user to choose a drink
 	public static String drinkChoice() {
 	    return getChoice("Que souhaitez-vous comme boissons :", new String[] {
 	        "Eau Plate", "Eau Gazeuse", "Soda", "Vin", "Aucune"
 	    });
 	}
 	
+	// // This method prompts the user to choose a dessert
 	public static String dessertChoice() {
 	    return getChoice("Que désirez-vous comme dessert :", new String[] {
 	        "Tarte Maison", "Mousse au chocolat", "Tiramisu", "Glaçes", "Aucun"
