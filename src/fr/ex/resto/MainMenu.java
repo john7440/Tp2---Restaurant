2b1e6a3e20358cd524f5f3047a37d87064af2a03
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-31 13:18:01 +0100

    ajout de sécurisation d'input utilisateur (pour le nombre de menu souhaité)

--- src/fr/ex/resto/MainMenu.java
+++ src/fr/ex/resto/MainMenu.java
@@ -12,41 +12,44 @@ public class MainMenu {
 		System.out.println("Bonjour, combien de menus désirez vous commander? ");
 		Scanner scan = new Scanner(System.in);
 		
-		int choice = scan.nextInt();
-		scan.nextLine();
-		
-		// Create a list to store all the menu orders
-		List<String> listOrders = new ArrayList<>();
-		
-		// Loop through the number of menus requested and collect user choices
-		for (int i = 1; i < choice +1; i++) {
-			System.out.println("\n============Menu #" + i +"============");
+		if (scan.hasNextInt()) {
+			int choice = scan.nextInt();
+			scan.nextLine();
 			
-			// Collect individual choices for each part of the menu
-			String entry = Menus.appetizerChoice();
-			String mainCourse = Menus.mainCourseChoice();
-			String side= Menus.sideChoice();
-			String drink = Menus.drinkChoice();
-			String dessert = Menus.dessertChoice();
+			// Create a list to store all the menu orders
+			List<String> listOrders = new ArrayList<>();
 			
-			// Format the menu summary
-			String order = "------Menu #" + i + "------" + "\n"
-                    + "- Entrée : " + entry + "\n"
-                    + "- Plat : " + mainCourse + "\n"
-                    + "- Accompagnement : " + side + "\n"
-                    + "- Boisson : " + drink + "\n"
-                    + "- Dessert : " + dessert;
+			// Loop through the number of menus requested and collect user choices
+			for (int i = 1; i < choice +1; i++) {
+				System.out.println("\n============Menu #" + i +"============");
+				
+				// Collect individual choices for each part of the menu
+				String entry = Menus.appetizerChoice();
+				String mainCourse = Menus.mainCourseChoice();
+				String side= Menus.sideChoice();
+				String drink = Menus.drinkChoice();
+				String dessert = Menus.dessertChoice();
+				
+				// Format the menu summary
+				String order = "------Menu #" + i + "------" + "\n"
+	                    + "- Entrée : " + entry + "\n"
+	                    + "- Plat : " + mainCourse + "\n"
+	                    + "- Accompagnement : " + side + "\n"
+	                    + "- Boisson : " + drink + "\n"
+	                    + "- Dessert : " + dessert;
+				
+				// Add the formatted menu to the list of orders
+	            listOrders.add(order);
+			}
 			
-			// Add the formatted menu to the list of orders
-            listOrders.add(order);
+			// Display the summary of all orders
+			System.out.println("\nRécapitulatif des commandes: ");
+			for (String order : listOrders) {
+	            System.out.println("\n" + order);
+	        }
+		} else {
+			System.out.println("Je n'ai pas saisi votre demande! Veuillez saisir un nombre entier!");
 		}
-		
-		// Display the summary of all orders
-		System.out.println("\nRécapitulatif des commandes: ");
-		for (String order : listOrders) {
-            System.out.println("\n" + order);
-        }
-		
 		scan.close();
 		
 	}
